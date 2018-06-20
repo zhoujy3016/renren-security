d002f6b1eddc3e5d4b1682e160e0ed661dd04819
zhoujy <zhoujy@zhoujy-PC>
2018-06-20 14:20:14 +0800

    插入测评的同时，插入测评与试题的关系数据

---
+++ evaluation_admin/src/main/java/io/renren/modules/sys/dao/BteEvalrefquestionDao.java
@@ -0,0 +1,15 @@
+package io.renren.modules.sys.dao;
+
+import io.renren.modules.sys.entity.BteEvalrefquestionEntity;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * 
+ * 
+ * @author chenshun
+ * @email sunlightcs@gmail.com
+ * @date 2018-06-20 13:52:32
+ */
+public interface BteEvalrefquestionDao extends BaseMapper<BteEvalrefquestionEntity> {
+	
+}
--- evaluation_admin/src/main/java/io/renren/modules/sys/service/impl/BteEvaluateServiceImpl.java
+++ evaluation_admin/src/main/java/io/renren/modules/sys/service/impl/BteEvaluateServiceImpl.java
@@ -2,8 +2,11 @@ package io.renren.modules.sys.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Map;
 import com.baomidou.mybatisplus.mapper.EntityWrapper;
 import com.baomidou.mybatisplus.plugins.Page;
@@ -15,9 +18,13 @@ import io.renren.common.utils.PageUtils;
 import io.renren.common.utils.Query;
 
 import io.renren.modules.sys.dao.BteEvaluateDao;
+import io.renren.modules.sys.entity.BteEvalrefquestionEntity;
 import io.renren.modules.sys.entity.BteEvaluateEntity;
+import io.renren.modules.sys.entity.BteQuestionEntity;
 import io.renren.modules.sys.entity.SysDictEntity;
+import io.renren.modules.sys.service.BteEvalrefquestionService;
 import io.renren.modules.sys.service.BteEvaluateService;
+import io.renren.modules.sys.service.BteQuestionService;
 import io.renren.modules.sys.service.SysDictService;
 import io.renren.modules.sys.shiro.ShiroUtils;
 
@@ -27,6 +34,12 @@ public class BteEvaluateServiceImpl extends ServiceImpl<BteEvaluateDao, BteEvalu
 
 	@Autowired
 	private SysDictService sysDictService;
+	
+	@Autowired
+	private BteQuestionService bteQuestionService;
+	
+	@Autowired
+	private BteEvalrefquestionService bteEvalrefquestionService;
 	
     @Override
     @DataCreaterFilter
@@ -48,10 +61,28 @@ public class BteEvaluateServiceImpl extends ServiceImpl<BteEvaluateDao, BteEvalu
     }
 
 	@Override
+	@Transactional(rollbackFor = Exception.class)
 	public void insertEvaluate(BteEvaluateEntity bteEvaluate) {
+		// 查询当前开启状态的试题
+		List<BteQuestionEntity> questionList = bteQuestionService.selectList(new EntityWrapper<BteQuestionEntity>().eq("question_state_id", "1"));
+		// 创建日期
 		bteEvaluate.setCreateDate(new Date());
+		// 创建人
 		bteEvaluate.setCreateUserId(ShiroUtils.getUserId());
 		this.insert(bteEvaluate);
+		Integer evalId = bteEvaluate.getDataNo();
+		
+		// 测评与试题关联数据
+		List<BteEvalrefquestionEntity> refList = new ArrayList<>();
+		
+		for(BteQuestionEntity question:questionList) {
+			BteEvalrefquestionEntity ref = new BteEvalrefquestionEntity();
+			ref.setEvalId(evalId);
+			ref.setQuestionId(question.getDataNo());
+			refList.add(ref);
+		}
+		// 批量插入关联数据
+		this.bteEvalrefquestionService.insertBatch(refList);
 	}
 
 }
