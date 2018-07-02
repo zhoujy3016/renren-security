73bcc896639dca9c6fdfe23e04850855f81b2980
zhoujy3016 <32054259+zhoujy3016@users.noreply.github.com>
2018-07-02 09:38:59 +0800

    优化结构

--- renren-common/src/main/java/io/renren/common/service/ExtraDictService.java
+++ renren-common/src/main/java/io/renren/common/service/ExtraDictService.java
@@ -22,28 +22,6 @@ public class ExtraDictService {
 	private SqlSession sqlSession;
 	
 	private Map<String, Object> extraMap;
-	
-	public void init() {
-		System.out.println("配置文件形式加载数据字典bean初始化");
-		extraMap = new HashMap<>();
-		Map<String, String> sqlMap = dictYmlConfig.getExtraDict();
-		System.out.println("####extra-dict start:");
-		for (String keys : sqlMap.keySet()) {
-			String sql = sqlMap.get(keys);
-			List<?> extraDictList = this.sqlSession.selectList(dictYmlConfig.getStatement(), sql);
-			extraMap.put(keys, extraDictList);
-		}
-		System.out.println("####extra-dict end:");
-	}
-	
-	public ExtraDictService() {
-		super();
-		
-	}
-	
-	public void destory() {
-		System.out.println("配置文件形式加载数据字典bean销毁");
-	}
 
 	public Map<String, Object> getExtraMap() {
 		return extraMap;
@@ -53,4 +31,40 @@ public class ExtraDictService {
 		this.extraMap = extraMap;
 	}
 
+	/**
+	 * init方法
+	 */
+	private void init() {
+		System.out.println("配置文件形式加载数据字典bean初始化");
+		extraMap = new HashMap<>();
+		Map<String, String> sqlMap = dictYmlConfig.getExtraDict();
+		System.out.println("####extra-dict start:");
+		for (String keys : sqlMap.keySet()) {
+			String sql = sqlMap.get(keys);
+			List<?> extraDictList = this.excuteQuery(sql);
+			extraMap.put(keys, extraDictList);
+		}
+		System.out.println("####extra-dict end:");
+	}
+	
+	public ExtraDictService() {
+		super();
+		
+	}
+
+	/**
+	 * destroy
+	 */
+	private void destory() {
+		System.out.println("配置文件形式加载数据字典bean销毁");
+	}
+
+	/**
+	 * 执行sql查询
+	 * @param sql
+	 * @return
+	 */
+	private List<?> excuteQuery(String sql) {
+		return this.sqlSession.selectList(dictYmlConfig.getStatement(), sql);
+	}
 }
