acfdba10401a781a4f4b71632f98161c1d024e1e
zhoujy3016 <32054259+zhoujy3016@users.noreply.github.com>
2018-08-16 09:34:28 +0800

    代码生成器修复

--- renren-generator/src/main/java/io/renren/controller/SysGeneratorController.java
+++ renren-generator/src/main/java/io/renren/controller/SysGeneratorController.java
@@ -1,6 +1,5 @@
 package io.renren.controller;
 
-import com.alibaba.fastjson.JSON;
 import io.renren.service.SysGeneratorService;
 import io.renren.utils.PageUtils;
 import io.renren.utils.Query;
@@ -12,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
@@ -20,7 +18,7 @@ import java.util.Map;
 
 /**
  * 代码生成器
- * 
+ *
  * @author chenshun
  * @email sunlightcs@gmail.com
  * @date 2016年12月19日 下午9:12:58
@@ -30,7 +28,7 @@ import java.util.Map;
 public class SysGeneratorController {
 	@Autowired
 	private SysGeneratorService sysGeneratorService;
-	
+
 	/**
 	 * 列表
 	 */
@@ -41,28 +39,24 @@ public class SysGeneratorController {
 		Query query = new Query(params);
 		List<Map<String, Object>> list = sysGeneratorService.queryList(query);
 		int total = sysGeneratorService.queryTotal(query);
-		
+
 		PageUtils pageUtil = new PageUtils(list, total, query.getLimit(), query.getPage());
-		
+
 		return R.ok().put("page", pageUtil);
 	}
-	
+
 	/**
 	 * 生成代码
 	 */
 	@RequestMapping("/code")
-	public void code(HttpServletRequest request, HttpServletResponse response) throws IOException{
-		String[] tableNames = new String[]{};
-		String tables = request.getParameter("tables");
-		tableNames = JSON.parseArray(tables).toArray(tableNames);
-		
-		byte[] data = sysGeneratorService.generatorCode(tableNames);
-		
-		response.reset();  
-        response.setHeader("Content-Disposition", "attachment; filename=\"renren.zip\"");  
-        response.addHeader("Content-Length", "" + data.length);  
-        response.setContentType("application/octet-stream; charset=UTF-8");  
-  
-        IOUtils.write(data, response.getOutputStream());  
+	public void code(String tables, HttpServletResponse response) throws IOException{
+		byte[] data = sysGeneratorService.generatorCode(tables.split(","));
+
+		response.reset();
+		response.setHeader("Content-Disposition", "attachment; filename=\"renren.zip\"");
+		response.addHeader("Content-Length", "" + data.length);
+		response.setContentType("application/octet-stream; charset=UTF-8");
+
+		IOUtils.write(data, response.getOutputStream());
 	}
 }
