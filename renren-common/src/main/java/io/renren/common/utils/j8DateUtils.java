b36532f25854df60e0b3481f8ebaadced615a073
zhoujy3016 <32054259+zhoujy3016@users.noreply.github.com>
2018-09-18 14:32:52 +0800

    完善工具类

--- renren-common/src/main/java/io/renren/common/utils/j8DateUtils.java
+++ renren-common/src/main/java/io/renren/common/utils/j8DateUtils.java
@@ -60,4 +60,93 @@ public class j8DateUtils {
         return new LocalDate[]{beginDate, endDate};
     }
 
+
+    /**
+     * 对日期的【秒】进行加/减
+     *
+     * @param localDateTime 日期
+     * @param seconds 秒数，负数为减
+     * @return 加/减几秒后的日期
+     */
+    public static LocalDateTime addDateSeconds(LocalDateTime localDateTime, int seconds) {
+        return localDateTime.plusSeconds(seconds);
+    }
+
+    /**
+     * 对日期的【分钟】进行加/减
+     *
+     * @param localDateTime 日期
+     * @param minutes 分钟数，负数为减
+     * @return 加/减几分钟后的日期
+     */
+    public static LocalDateTime addDateMinutes(LocalDateTime localDateTime, int minutes) {
+        return localDateTime.plusMinutes(minutes);
+    }
+
+
+    /**
+     * 对日期的【小时】进行加/减
+     *
+     * @param localDateTime 日期
+     * @param hours 小时数，负数为减
+     * @return 加/减几小时后的日期
+     */
+    public static LocalDateTime addDateHours(LocalDateTime localDateTime, int hours) {
+        return localDateTime.plusHours(hours);
+    }
+
+    /**
+     * 对日期的【天】进行加/减
+     *
+     * @param localDateTime 日期
+     * @param days 天数，负数为减
+     * @return 加/减几天后的日期
+     */
+    public static LocalDateTime addDateDays(LocalDateTime localDateTime, int days) {
+        return localDateTime.plusDays(days);
+    }
+
+    /**
+     * 对日期的【周】进行加/减
+     *
+     * @param localDateTime 日期
+     * @param weeks 周数，负数为减
+     * @return 加/减几周后的日期
+     */
+    public static LocalDateTime addDateWeeks(LocalDateTime localDateTime, int weeks) {
+        return localDateTime.plusWeeks(weeks);
+    }
+
+    /**
+     * 对日期的【月】进行加/减
+     *
+     * @param localDateTime 日期
+     * @param months 月数，负数为减
+     * @return 加/减几月后的日期
+     */
+    public static LocalDateTime addDateMonths(LocalDateTime localDateTime, int months) {
+        return localDateTime.plusMonths(months);
+    }
+
+    /**
+     * 对日期的【年】进行加/减
+     *
+     * @param localDateTime 日期
+     * @param years 年数，负数为减
+     * @return 加/减几年后的日期
+     */
+    public static LocalDateTime addDateYears(LocalDateTime localDateTime, int years) {
+        return localDateTime.plusYears(years);
+    }
+
+    /**
+     * test
+     * @param args
+     */
+    public static void main(String[] args) {
+        String dateTime = format(addDateYears(stringToDate("2013-09-18 14:30:00", DATE_TIME_PATTERN), 5), DATE_TIME_PATTERN);
+        System.out.println(dateTime);
+    }
+
+
 }
