7f9225953e110cbe7b981e25bb3284e9935b0988
zhoujy3016 <32054259+zhoujy3016@users.noreply.github.com>
2018-10-29 08:58:11 +0800

    优化

--- renren-common/src/main/java/io/renren/common/utils/CookieUtils.java
+++ renren-common/src/main/java/io/renren/common/utils/CookieUtils.java
@@ -14,14 +14,16 @@ public class CookieUtils {
      * @param request
      * @return
      */
+    public static final String KEY_SESSION = "JSESSIONID";
+
     public static String getSessionId(HttpServletRequest request) {
-        Map<String, Cookie> cookieMap = new HashMap<>();
+        Map<String, Cookie> cookieMap = new HashMap<>(10);
         //获取所有的cookie值
         Cookie[] cookies = request.getCookies();
         if(cookies != null){
             Arrays.stream(cookies).forEach(cookie -> cookieMap.put(cookie.getName(),cookie));
-            if(cookieMap.containsKey("JSESSIONID")) {
-                return ((Cookie)cookieMap.get("JSESSIONID")).getValue();
+            if(cookieMap.containsKey(KEY_SESSION)) {
+                return cookieMap.get(KEY_SESSION).getValue();
             }
         }
         return StringUtils.EMPTY;
