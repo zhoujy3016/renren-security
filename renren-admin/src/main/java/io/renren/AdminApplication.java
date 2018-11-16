8f7b7bf43774492a6994c51edc584176432e0a96
zhoujy3016 <zhoujunyi-110@163.com>
2018-11-16 18:44:28 +0800

    activiti自动配置及部署流程

--- renren-admin/src/main/java/io/renren/AdminApplication.java
+++ renren-admin/src/main/java/io/renren/AdminApplication.java
@@ -1,12 +1,13 @@
 package io.renren;
 
+import org.activiti.spring.boot.SecurityAutoConfiguration;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @MapperScan(basePackages = {"io.renren.modules.*.dao"})
 public class AdminApplication extends SpringBootServletInitializer {
 	
---
+++ renren-admin/src/main/java/io/renren/common/config/ActivitiConfig.java
@@ -0,0 +1,30 @@
+package io.renren.common.config;
+
+import org.activiti.engine.ProcessEngine;
+import org.activiti.engine.ProcessEngineConfiguration;
+import org.activiti.engine.impl.cfg.StandaloneProcessEngineConfiguration;
+import org.activiti.spring.boot.AbstractProcessEngineAutoConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
+
+public class ActivitiConfig extends AbstractProcessEngineAutoConfiguration {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Bean
+    public StandaloneProcessEngineConfiguration processEngineConfiguration() {
+        StandaloneProcessEngineConfiguration configuration = new StandaloneProcessEngineConfiguration();
+        configuration.setDataSource(dataSource);
+        configuration.setDatabaseSchemaUpdate(ProcessEngineConfiguration.DB_SCHEMA_UPDATE_TRUE);
+        configuration.setAsyncExecutorActivate(false);
+        return configuration;
+    }
+
+    @Bean
+    public ProcessEngine processEngine() {
+        return processEngineConfiguration().buildProcessEngine();
+    }
+}
