72791eb6cbfe721ba8a7bc5da8ba10d0533ec86c
zhoujy3016 <32054259+zhoujy3016@users.noreply.github.com>
2018-12-10 15:45:13 +0800

    去掉无用代码

--- evaluation_admin/src/main/java/io/renren/common/component/AppRunner.java
+++ evaluation_admin/src/main/java/io/renren/common/component/AppRunner.java
@@ -1,22 +1,16 @@
-//package io.renren.common.component;
-//
-//import io.renren.dictionary.component.DictComponent;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.ApplicationArguments;
-//import org.springframework.boot.ApplicationRunner;
-//import org.springframework.stereotype.Component;
-//
-//
-//@Component
-//public class AppRunner implements ApplicationRunner{
-//
-//	@Autowired
-//	private DictComponent dictComponent;
-//
-//	@Override
-//	public void run(ApplicationArguments args) throws Exception {
-//		// 加载数据字典到redis中
-//		dictComponent.initDictCacheData();
-//	}
-//
-//}
+package io.renren.common.component;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+
+@Component
+public class AppRunner implements ApplicationRunner{
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+
+	}
+
+}
