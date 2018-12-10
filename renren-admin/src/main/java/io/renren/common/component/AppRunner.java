9f03b3096f80bd6363222551f26058ac55529b36
zhoujy3016 <32054259+zhoujy3016@users.noreply.github.com>
2018-12-10 15:09:23 +0800

    启动类放到dictionary模块中，进一步解耦。

--- renren-admin/src/main/java/io/renren/common/component/AppRunner.java
+++ renren-admin/src/main/java/io/renren/common/component/AppRunner.java
@@ -1,22 +1,22 @@
-package io.renren.common.component;
-
-import io.renren.dictionary.component.DictComponent;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.stereotype.Component;
-
-
-@Component
-public class AppRunner implements ApplicationRunner{
-
-	@Autowired
-	private DictComponent dictComponent;
-
-	@Override
-	public void run(ApplicationArguments args) throws Exception {
-		// 加载数据字典到redis中
-		dictComponent.initDictCacheData();
-	}
-	
-}
+//package io.renren.common.component;
+//
+//import io.renren.dictionary.component.DictComponent;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.ApplicationArguments;
+//import org.springframework.boot.ApplicationRunner;
+//import org.springframework.stereotype.Component;
+//
+//
+//@Component
+//public class AppRunner implements ApplicationRunner{
+//
+//	@Autowired
+//	private DictComponent dictComponent;
+//
+//	@Override
+//	public void run(ApplicationArguments args) throws Exception {
+//		// 加载数据字典到redis中
+//		dictComponent.initDictCacheData();
+//	}
+//
+//}
