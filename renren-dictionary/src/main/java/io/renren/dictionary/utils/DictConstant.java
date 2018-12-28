13c4c8e52be3be34ee98ee34bc1e4614eb202400
zhoujy3016 <32054259+zhoujy3016@users.noreply.github.com>
2018-12-28 15:44:19 +0800

    引用常量

--- renren-dictionary/src/main/java/io/renren/dictionary/utils/DictConstant.java
+++ renren-dictionary/src/main/java/io/renren/dictionary/utils/DictConstant.java
@@ -79,7 +79,7 @@ public class DictConstant {
      * @return
      */
     public static String getDictionaryKey(String key){
-        return "dictionary:" + key;
+        return DICT_PREFIX + key;
     }
 
 }
