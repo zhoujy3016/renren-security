8ea15795aa364dad039e01bd169f3bf58d87cee4
zhoujy3016 <zhoujunyi-110@163.com>
2019-02-01 07:43:49 +0800

    优化逻辑

--- renren-dictionary/src/main/java/io/renren/dictionary/conditional/DictYmlCondition.java
+++ renren-dictionary/src/main/java/io/renren/dictionary/conditional/DictYmlCondition.java
@@ -1,5 +1,6 @@
 package io.renren.dictionary.conditional;
 
+import org.apache.commons.lang.StringUtils;
 import org.springframework.context.annotation.Condition;
 import org.springframework.context.annotation.ConditionContext;
 import org.springframework.core.type.AnnotatedTypeMetadata;
@@ -9,12 +10,8 @@ public class DictYmlCondition implements Condition {
 	@Override
 	public boolean matches(ConditionContext context, AnnotatedTypeMetadata metadata) {
 		String str = context.getEnvironment().getProperty("use-dict-yml-default");
-		// 配置文件中，没有此项定义，默认为true
-		if(str == null) {
-			return true;
-		} else {
-			return str.equals("true");
-		}
+		// 配置文件中，未定义或为空，默认为true
+		return StringUtils.isBlank(str) || str.equals("true");
 	}
 
 }
