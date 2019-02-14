70a9579729030db2c7d793838d129ef3db074aec
zhoujy3016 <32054259+zhoujy3016@users.noreply.github.com>
2019-02-14 10:54:07 +0800

    常用配置

--- renren-admin/src/main/java/io/renren/common/config/WebConfig.java
+++ renren-admin/src/main/java/io/renren/common/config/WebConfig.java
@@ -4,11 +4,14 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 import io.renren.common.utils.J8DateUtils;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerViewResolver;
 
 import java.text.SimpleDateFormat;
 import java.util.List;
@@ -25,21 +28,6 @@ public class WebConfig implements WebMvcConfigurer {
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/statics/**").addResourceLocations("classpath:/statics/");
     }
-
-//    @Override
-//    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
-//        MappingJackson2HttpMessageConverter jackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
-//        ObjectMapper objectMapper = jackson2HttpMessageConverter.getObjemctMapper();
-//
-//        //生成json时，将所有Long转换成String
-//        SimpleModule simpleModule = new SimpleModule();
-//        simpleModule.addSerializer(Long.class, ToStringSerializer.instance);
-//        simpleModule.addSerializer(Long.TYPE, ToStringSerializer.instance);
-//        objectMapper.registerModule(simpleModule);
-//
-//        jackson2HttpMessageConverter.setObjectMapper(objectMapper);
-//        converters.add(0, jackson2HttpMessageConverter);
-//    }
 
     @Override
     public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
@@ -59,4 +47,29 @@ public class WebConfig implements WebMvcConfigurer {
         jackson2HttpMessageConverter.setObjectMapper(objectMapper);
         converters.add(0, jackson2HttpMessageConverter);
     }
+
+
+//    /**
+//     * 代码配置视图解析器
+//     * @return
+//     */
+//    @Bean
+//    public FreeMarkerViewResolver freeMarkerViewResolver() {
+//        FreeMarkerViewResolver resolver = new FreeMarkerViewResolver();
+//        resolver.setContentType("text/html;charset=UTF-8");
+//        resolver.setSuffix(".html");
+//        resolver.setRequestContextAttribute("request");
+//        return resolver;
+//    }
+//
+
+//    /**
+//     * 自定义参数解析器
+//     * @param resolvers
+//     */
+//    @Override
+//    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers){
+//        resolvers.add(new SessionArgumentResolver());
+//    }
+
 }
