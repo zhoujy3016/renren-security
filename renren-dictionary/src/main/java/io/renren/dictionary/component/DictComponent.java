540091de28d322e0f4198d3ff190aceb0928ff3f
zhoujy3016 <32054259+zhoujy3016@users.noreply.github.com>
2019-03-21 15:18:58 +0800

    修改方法名称

--- renren-dictionary/src/main/java/io/renren/dictionary/component/DictComponent.java
+++ renren-dictionary/src/main/java/io/renren/dictionary/component/DictComponent.java
@@ -33,9 +33,9 @@ public class DictComponent {
      */
     public void initDictCacheData() {
 		// 数据字典载入到redis
-		loadDictDataToRedis(getMapByGroup(this.sysDictService.getAllSysDictEntity()));
+		loadDictDataToCache(getMapByGroup(this.sysDictService.getAllSysDictEntity()));
 		// 自定义的数据字典载入到redis
-		loadExtraDictDataToRedis(this.extraDictService.getExtraMap());
+		loadExtraDictDataToCache(this.extraDictService.getExtraMap());
     }
 
 	/**
@@ -50,8 +50,8 @@ public class DictComponent {
      * 数据放到redis数据库中
      * @param dictMapGroup
      */
-    private void loadDictDataToRedis(Map<String, List<Map<String, Object>>> dictMapGroup) {
-    	dictMapGroup.forEach((k, v) -> setDictMapToRedis(k, v));
+    private void loadDictDataToCache(Map<String, List<Map<String, Object>>> dictMapGroup) {
+    	dictMapGroup.forEach((k, v) -> setDictMapToCache(k, v));
     }
     
     /**
@@ -82,7 +82,7 @@ public class DictComponent {
     public void reloadDictCacheData(String type) {
     	// 查询当前type中的数据字典列表
 		List<Map<String, Object>> dictMapList = this.sysDictService.getSysDictEntity(type);
-		setDictMapToRedis(type, dictMapList);
+		setDictMapToCache(type, dictMapList);
     }
     
     /**
@@ -92,13 +92,13 @@ public class DictComponent {
     public void reloadDictCacheData(Long[] ids) {
     	// id数组查询数据字典集合并用type分组
 		Map<String, List<Map<String, Object>>> dictMapGroup = this.sysDictService.getSysDictEntityAfterDelete(ids);
-		loadDictDataToRedis(dictMapGroup);
+		loadDictDataToCache(dictMapGroup);
     }
 
     /**
      * 将配置文件中针对特殊的表需要放入数据字典的map,放入redis 数据字典缓存中
      */
-    private void loadExtraDictDataToRedis(Map<String, Object> extraMap) {
+    private void loadExtraDictDataToCache(Map<String, Object> extraMap) {
     	if(extraMap != null) {
 			extraMap.forEach((k, v) -> cacheController.set(k, v));
 		}
@@ -109,14 +109,14 @@ public class DictComponent {
 	 * @param extraMap
 	 */
 	public void reloadExtraCacheData(Map<String, Object> extraMap) {
-    	this.loadExtraDictDataToRedis(extraMap);
+    	this.loadExtraDictDataToCache(extraMap);
 	}
 
 	/**
 	 * 根据type查询对应的数据字典，set到redis中
 	 * @param type
 	 */
-	private void setDictMapToRedis(String type, List<Map<String, Object>> list) {
+	private void setDictMapToCache(String type, List<Map<String, Object>> list) {
 		cacheController.reset(type, list);
 	}
 }
