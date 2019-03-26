85d7d297251ea857d791827ab48097778c14416f
zhoujy3016 <32054259+zhoujy3016@users.noreply.github.com>
2019-03-26 11:03:54 +0800

    规范代码

--- renren-dictionary/src/main/java/io/renren/dictionary/config/DictionaryConfig.java
+++ renren-dictionary/src/main/java/io/renren/dictionary/config/DictionaryConfig.java
@@ -1,6 +1,7 @@
 package io.renren.dictionary.config;
 
 import io.renren.dictionary.aspect.strategy.ExtraDictModifyHandler;
+import io.renren.dictionary.aspect.strategy.IDictModifyHandler;
 import io.renren.dictionary.aspect.strategy.NormalDictModifyHandler;
 import io.renren.dictionary.service.ExtraDictService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -29,13 +30,13 @@ public class DictionaryConfig {
 
 	@Lazy
 	@Bean
-	NormalDictModifyHandler normalDictModifyHandler() {
+	IDictModifyHandler normalDictModifyHandler() {
 		return new NormalDictModifyHandler();
 	}
 
 	@Lazy
 	@Bean
-	ExtraDictModifyHandler extraDictModifyHandler() {
+	IDictModifyHandler extraDictModifyHandler() {
 		return new ExtraDictModifyHandler();
 	}
 }
