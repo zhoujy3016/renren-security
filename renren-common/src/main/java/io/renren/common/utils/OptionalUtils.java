f2b72572df571da94b6ea45d4367d92d1ddbf37b
zhoujy3016 <32054259+zhoujy3016@users.noreply.github.com>
2019-04-23 15:35:30 +0800

    增加string to long

--- renren-common/src/main/java/io/renren/common/utils/OptionalUtils.java
+++ renren-common/src/main/java/io/renren/common/utils/OptionalUtils.java
@@ -30,4 +30,28 @@ public class OptionalUtils {
                 .flatMap(OptionalUtils::s2i)
                 .orElse(0);
     }
+
+    /**
+     * 将字符串转换为Long并返回Optional<Long>
+     * @param s
+     * @return
+     */
+    public static Optional<Long> s2l(String s) {
+        try {
+            return Optional.of(Long.parseLong(s));
+        } catch(NumberFormatException e) {
+            return Optional.empty();
+        }
+    }
+
+    /**
+     * 调用字符串转long，并给出一个默认值0
+     * @param s
+     * @return
+     */
+    public static long stringToLong(String s) {
+        return Optional.ofNullable(s)
+                .flatMap(OptionalUtils::s2l)
+                .orElse(0L);
+    }
 }
